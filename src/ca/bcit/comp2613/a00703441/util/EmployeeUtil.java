1068bfc090132556ae6653c6b8de7050cb6a6101
wchou33 <wchou_33@hotmail.com>
2014-05-21 21:48:38 -0700

    added util package and test driver

---
+++ src/ca/bcit/comp2613/a00703441/util/EmployeeUtil.java
@@ -0,0 +1,61 @@
+package ca.bcit.comp2613.a00703441.util;
+
+import java.util.ArrayList;
+
+import ca.bcit.comp2613.carsales.model.Employee;
+
+public class EmployeeUtil {
+
+	public static String LOREM_IPSUM = "In publishing and graphic design, lorem ipsum is a placeholder text commonly used to "
+			+ "demonstrate the graphic elements of a document or visual presentation. By replacing the distraction of meaningful "
+			+ "content with filler text of scrambled Latin it allows viewers to focus on graphical elements such as font, typography, and layout"
+			+ "The lorem ipsum text is typically a mangled section of De finibus bonorum et malorum, a 1st-century BC Latin text by Cicero, with words "
+			+ "altered, added, and removed that make it nonsensical, improper Latin"
+			+ "A variation of the common lorem ipsum text has been used during typesetting since the 1960s or "
+			+ "earlier, when it was popularized by advertisements for Letraset transfer sheets. It was introduced "
+			+ "to the Digital Age by Aldus Corporation in the mid-1980s, which employed it in graphics and word processing templates "
+			+ "for its breakthrough desktop publishing program, PageMaker for the Apple Macintosh";
+
+	public static ArrayList<Employee> createEmployees() {
+		ArrayList<Employee> retval = new ArrayList<>();
+		String[] strs = LOREM_IPSUM.split("\\s");
+
+		for (int i = 0; i < strs.length; i++) {
+			Employee employee = new Employee();
+			employee.setId(Integer.toString(i));
+			employee.setFirstName(strs[i].toUpperCase());
+			employee.setLastName(new StringBuilder(strs[i]).reverse()
+					.toString());
+			retval.add(employee);
+		}
+		return retval;
+	}
+
+	public static void printEmployees(ArrayList<Employee> employees) {
+		for (Employee employee : employees) {
+			System.out.println(employee);
+		}
+	}
+
+	public static ArrayList<Employee> searchEmployeesByLastName(
+			ArrayList<Employee> employees, String lastName) {
+		ArrayList<Employee> retval = new ArrayList<>();
+		for (Employee employee : employees) {
+			if (employee.getLastName().equals(lastName)) {
+				retval.add(employee);
+			}
+		}
+		return retval;
+	}
+	
+	public static ArrayList<Employee> searchEmployeesByLastNameRegex(ArrayList<Employee> employees, String regex) {
+		ArrayList<Employee> retval = new ArrayList<>();
+		for (Employee employee : employees) {
+			if (employee.getLastName().matches(regex)) {
+				System.out.println(employee);
+			}
+		}
+		return retval;
+	}
+
+}
--- src/ca/bcit/comp2613/carsales/TestDriver.java
+++ src/ca/bcit/comp2613/carsales/TestDriver.java
@@ -1,9 +1,24 @@
 package ca.bcit.comp2613.carsales;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import ca.bcit.comp2613.carsales.model.Employee;
+import ca.bcit.comp2613.a00703441.util.EmployeeUtil;
+
 public class TestDriver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<Employee> employees = EmployeeUtil.createEmployees();
+		EmployeeUtil.printEmployees(employees);
+		System.out.println("------------------");
+		ArrayList<Employee> employeesByLastName = EmployeeUtil.searchEmployeesByLastName(employees, "PUBLISHING");
+		EmployeeUtil.printEmployees(employeesByLastName);
+		
+		System.out.println("------------------");
+		ArrayList<Employee> employeesByLastNameRegex = EmployeeUtil.searchEmployeesByLastNameRegex(employees, "WO.*");
+		EmployeeUtil.printEmployees(employeesByLastNameRegex);
 
 	}
 
